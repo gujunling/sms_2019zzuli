f2e91169b443ed98aa8a878b9ee5b4fb453c1ff7
gujunling <gujunq1998@163.com>
2019-06-06 12:59:33 +0800

    修改课程信息

---
+++ src/main/java/com/briup/apps/app02/bean/extend/StudentCourseExtend.java
@@ -0,0 +1,9 @@
+package com.briup.apps.app02.bean.extend;
+
+/**
+ * @author gujunqi
+ * @create 2019-06-06-12:37
+ */
+public class StudentCourseExtend {
+
+}
---
+++ src/main/java/com/briup/apps/app02/dao/extend/StudentCourseExtendMapper.java
@@ -0,0 +1,9 @@
+package com.briup.apps.app02.dao.extend;
+
+/**
+ * @author gujunqi
+ * @create 2019-06-06-12:37
+ */
+public interface StudentCourseExtendMapper {
+
+}
